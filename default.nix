46fc56d327e710e3d5aa939c9087c7e207225228
Sibi Prabakaran <sibi@psibi.in>
2016-03-13 23:17:49 +0530

    Built file with the private package

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7103" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./persistent-try-bugs.nix { }
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./persistent-try-bugs.nix { persistent-postgresql = nixpkgs.haskellPackages.persistent-postgresql; }
